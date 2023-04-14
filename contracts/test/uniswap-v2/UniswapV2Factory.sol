d4b65091b509f4c63f79737bba8ef6bb4acddff6
Sooyoung Lee <dannylisa67@gmail.com>
2023-04-14 23:52:24 +0900

    use uniswap library

---
+++ contracts/test/uniswap-v2/UniswapV2Factory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC20/ERC20.sol)
+
+pragma solidity =0.5.16;
+
+import "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
+
+contract V2Factory is UniswapV2Factory {}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      { version: "0.8.18" },
+      { version: "0.6.6" },
+      { version: "0.5.16" },
+    ],
+  },
 };
 
 export default config;
