957a313ceaebe03325480d144e3a43b9cc55dd4b
winterjihwan <113398351+winterjihwan@users.noreply.github.com>
2023-04-15 01:36:11 +0900

    added: state variables, constructor

--- contracts/core/uniswap-v2/UniswapV2Defuture.sol
+++ contracts/core/uniswap-v2/UniswapV2Defuture.sol
@@ -13,4 +13,17 @@ abstract contract UniswapV2Defuture is BaseDefuture, IUniswapV2Defuture {
     address public token0;
     address public token1;
     address public pair;
+    address public WETH;
+
+    function addPosition(address buyToken, address sellToken, address to, uint margin, uint deadline) public {}
+
+    function closePosition(uint positionId, address to, uint deadline) public {}
+
+    constructor(
+        uint16 _minMarginBps,
+        uint16 _liquidateFactorBps,
+        uint16 _liquidatePaybackBps,
+        address _pair,
+        address _WETH
+    ) BaseDefuture("UniswapV2 Defuture", "UNI2DF", _minMarginBps, _liquidateFactorBps, _liquidatePaybackBps) {}
 }
--- test/Test.ts
+++ test/Test.ts
@@ -3,6 +3,4 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Empty Test", function () {
-  
-});
+describe("Empty Test", function () {});
