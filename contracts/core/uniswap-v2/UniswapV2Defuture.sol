9eb0de72d872397b726f639950473c8ac77154d2
winterjihwan <113398351+winterjihwan@users.noreply.github.com>
2023-04-15 09:59:04 +0900

    added: mint/burn range nft functions, altered addPosition functionality for defutures

--- contracts/core/uniswap-v2/UniswapV2Defuture.sol
+++ contracts/core/uniswap-v2/UniswapV2Defuture.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "../BaseDefuture.sol";
 import "../../interfaces/core/uniswap-v2/IUniswapV2Defuture.sol";
 import "../../uniswap-v2/core/interfaces/IUniswapV2Pair.sol";
+import "../../interfaces/core/uniswap-v2/IUniswapV2DefutureFactory.sol";
 import "../../libraries/SafeToken.sol";
 
 abstract contract UniswapV2Defuture is BaseDefuture, IUniswapV2Defuture {
@@ -15,8 +16,8 @@ abstract contract UniswapV2Defuture is BaseDefuture, IUniswapV2Defuture {
     address public token0;
     address public token1;
     address public pair;
-    address public factory;
     address public WETH;
+    address public factory;
     uint112 private leading0;
     uint112 private leading1;
     uint32 private timestampLastSync;
@@ -35,13 +36,47 @@ abstract contract UniswapV2Defuture is BaseDefuture, IUniswapV2Defuture {
         factory = msg.sender;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == IUniswapV2DefutureFactory(factory).owner(), "only owner");
+        _;
+    }
+    modifier onlyLiquidator() {
+        require(isLiquidator[msg.sender] == true, "only liquidator");
+        _;
+    }
+
+    function getAmountIn(
+        uint112 amountOut,
+        uint112 reserveIn,
+        uint112 reserveOut
+    ) internal pure returns (uint112 amountIn) {
+        uint numerator = uint(reserveIn) * amountOut * 1000;
+        uint denominator = (reserveOut - amountOut) * 997; // swap 하려고 넣은 토큰의 0.03% 수수료를 뺀다.
+        amountIn = uint112(numerator / denominator + 1);
+    }
+
+    function getStrikeAmount(
+        uint112 _leadingBuy,
+        uint112 _leadingSell,
+        uint112 _amountBuy
+    ) public view returns (uint112) {
+        // 20X -> 100Y
+        // 100Y ->
+        return
+            ((getAmountIn(_amountBuy * 2, _leadingSell, _leadingBuy) -
+                getAmountIn(_amountBuy, _leadingSell, _leadingBuy)) * 1000) / 997;
+    }
+
     function addPosition(address to, bool buy0, uint112 amountBuy, uint112 marginWithFee) public override {
         Slot0 memory _slot0 = slot0;
 
         (uint112 _leadingSell, uint112 _leadingBuy) = sync();
+        if (buy0) (_leadingSell, _leadingBuy) = (_leadingBuy, _leadingSell);
 
-        // uint112 strike = getStrikeAmount(_leadingBuy, _leadingSell, amountBuy);
-        // TODO FIX
+        // 100Y를 살 수 있는 권리를 사고 싶은데, 얼마야?
+        // -> 선물 가격
+
+        uint112 strike = getStrikeAmount(_leadingBuy, _leadingSell, amountBuy);
         uint112 margin = (marginWithFee * 997) / 1000;
         require(margin >= (strike * _slot0.minMarginBps) / 1E4, "DEFUTURE: MARGIN SHORTAGE");
 
@@ -59,7 +94,7 @@ abstract contract UniswapV2Defuture is BaseDefuture, IUniswapV2Defuture {
             leading1 -= uint112(amountBuy);
         }
 
-        // _mintPosition(to, uint8(buy0 ? PositionType.BUY0 : PositionType.BUY1), margin, strike, amountBuy);
+        _mintPosition(to, uint8(buy0 ? PositionType.BUY0 : PositionType.BUY1), margin, strike, amountBuy);
         // NFT 민팅추가 기능
     }
 
