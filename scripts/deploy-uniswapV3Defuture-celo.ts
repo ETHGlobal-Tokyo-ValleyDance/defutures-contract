202223a95aa30283917e3e5d6b568a1ad28e60c6
suha jin <89185836+djm07073@users.noreply.github.com>
2023-04-16 07:31:03 +0900

    V3 test complete

--- scripts/deploy-uniswapV3Defuture-celo.ts
+++ scripts/deploy-uniswapV3Defuture-celo.ts
@@ -6,6 +6,105 @@ import {
   UniswapV3DefutureRouter,
   UniswapV3DefutureRouter__factory,
 } from "../typechain-types"
+const uniswapV3FactoryAbi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "uint24", name: "fee", type: "uint24" },
+      { indexed: true, internalType: "int24", name: "tickSpacing", type: "int24" },
+    ],
+    name: "FeeAmountEnabled",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "oldOwner", type: "address" },
+      { indexed: true, internalType: "address", name: "newOwner", type: "address" },
+    ],
+    name: "OwnerChanged",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "token0", type: "address" },
+      { indexed: true, internalType: "address", name: "token1", type: "address" },
+      { indexed: true, internalType: "uint24", name: "fee", type: "uint24" },
+      { indexed: false, internalType: "int24", name: "tickSpacing", type: "int24" },
+      { indexed: false, internalType: "address", name: "pool", type: "address" },
+    ],
+    name: "PoolCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "tokenA", type: "address" },
+      { internalType: "address", name: "tokenB", type: "address" },
+      { internalType: "uint24", name: "fee", type: "uint24" },
+    ],
+    name: "createPool",
+    outputs: [{ internalType: "address", name: "pool", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint24", name: "fee", type: "uint24" },
+      { internalType: "int24", name: "tickSpacing", type: "int24" },
+    ],
+    name: "enableFeeAmount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint24", name: "", type: "uint24" }],
+    name: "feeAmountTickSpacing",
+    outputs: [{ internalType: "int24", name: "", type: "int24" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint24", name: "", type: "uint24" },
+    ],
+    name: "getPool",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "parameters",
+    outputs: [
+      { internalType: "address", name: "factory", type: "address" },
+      { internalType: "address", name: "token0", type: "address" },
+      { internalType: "address", name: "token1", type: "address" },
+      { internalType: "uint24", name: "fee", type: "uint24" },
+      { internalType: "int24", name: "tickSpacing", type: "int24" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+    name: "setOwner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+]
 let minMarginBps = 100
 let liquidateFactorBps = 100
 let liquidatePaybackBps = 5000
@@ -51,7 +150,7 @@ async function main() {
   console.log("uniswapV3DefutureFactory deployed to:", uniswapV3DefutureFactory.address)
   console.log("deployer address:", deployer.address)
   //* Before create 3 DefuturePairs, we need to addPair to uniswapV3Factory
-  const uniswapV3Factory = await ethers.getContractAt("IUniswapV3Factory", uniswapV3FactoryAddr)
+  const uniswapV3Factory = await ethers.getContractAt(uniswapV3FactoryAbi, uniswapV3FactoryAddr)
   await uniswapV3Factory.createPool(t1.address, t2.address, fee)
   await uniswapV3Factory.createPool(t2.address, t3.address, fee)
   await uniswapV3Factory.createPool(t1.address, t3.address, fee)
