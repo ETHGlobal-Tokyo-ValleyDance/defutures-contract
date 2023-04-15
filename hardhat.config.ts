91fd92b1015135cbbb96cae60e47462b7ab503fc
Sooyoung Lee <dannylisa67@gmail.com>
2023-04-16 04:56:33 +0900

    add linea

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,8 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const SY_ACCOUNT = process.env.SY_ACCOUNT!;
-const JH_ACCOUNT = process.env.JH_ACCOUNT!;
+const SY_ACCOUNT = process.env.SY_ACCOUNT!
+const JH_ACCOUNT = process.env.JH_ACCOUNT!
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -81,13 +81,17 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       chainId: 31337,
-
     },
     scroll: {
       chainId: 534353,
-      url: 'https://alpha-rpc.scroll.io/l2',
+      url: "https://alpha-rpc.scroll.io/l2",
       accounts: [SY_ACCOUNT],
-    }
+    },
+    linea: {
+      chainId: 59140,
+      url: "https://rpc.goerli.linea.build",
+      accounts: [SY_ACCOUNT],
+    },
   },
 }
 
